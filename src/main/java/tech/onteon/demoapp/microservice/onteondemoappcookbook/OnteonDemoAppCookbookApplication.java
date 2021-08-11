3681661290bfcaaf5f1f8450e6977eb3302967a1
Patryk Borchowiec <patryk.borchowiec@jlupin.com>
2021-08-11 09:26:27 +0200

    Init

---
+++ src/main/java/tech/onteon/demoapp/microservice/onteondemoappcookbook/OnteonDemoAppCookbookApplication.java
@@ -0,0 +1,13 @@
+package tech.onteon.demoapp.microservice.onteondemoappcookbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnteonDemoAppCookbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnteonDemoAppCookbookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/onteon/demoapp/microservice/onteondemoappcookbook/OnteonDemoAppCookbookApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.onteon.demoapp.microservice.onteondemoappcookbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnteonDemoAppCookbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

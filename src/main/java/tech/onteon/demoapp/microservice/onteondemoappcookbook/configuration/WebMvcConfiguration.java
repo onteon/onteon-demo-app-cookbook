73d20b1a80b3dd920cee7cd58723d30e5b4f37c8
Patryk Borchowiec <patryk.borchowiec@jlupin.com>
2021-10-05 15:14:34 +0200

    Updated config files. Added printing success line.

--- src/main/java/tech/onteon/demoapp/microservice/onteondemoappcookbook/configuration/WebMvcConfiguration.java
+++ src/main/java/tech/onteon/demoapp/microservice/onteondemoappcookbook/configuration/WebMvcConfiguration.java
@@ -5,7 +5,9 @@
  */
 package tech.onteon.demoapp.microservice.onteondemoappcookbook.configuration;
 
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -36,5 +38,10 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
         registry
                 .addResourceHandler("/images/**")
                 .addResourceLocations("file:images/");
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void onApplicationReady() {
+        System.out.println("Spring Boot properly started");
     }
 }
